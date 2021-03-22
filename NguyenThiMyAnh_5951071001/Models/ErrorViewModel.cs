95143b887f6249e0ec3fa928a31e43e5c9b68617
nguyenmya <80749352+nguyenmya@users.noreply.github.com>
2021-03-22 20:01:09 +0700

    xong

---
+++ NguyenThiMyAnh_5951071001/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyenThiMyAnh_5951071001.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
